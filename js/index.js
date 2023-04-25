12d7f3bb0d633de72ddcb92a99327579cdb1f6b4
Vaishnavi Suryawanshi <vaishnavishruti121@gmail.com>
2023-04-25 16:40:33 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+console.log('Hello');
+console.log('My name is vaishnavi')
+// addition//
+console.log('4+2')
--- js/variables.js
+++ js/variables.js
@@ -1,7 +1,11 @@
-console.log("first program in javascript")
+/*console.log("first program in javascript")
 var  a=5; //we can write let also instead of var
 console.log(a)
 a="xyz";
 console.log(a)
 //xyz=5;
-//console.log(a)
+//console.log(a)*/
+var message;
+message="Hello vaishnavi"
+message="My name is vaishnavi"
+console.log(message)
