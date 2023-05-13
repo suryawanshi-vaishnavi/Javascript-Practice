e0b36b23da8e1151698f122ecc70012f4c6aa541
Vaishnavi Suryawanshi <vaishnavishruti121@gmail.com>
2023-05-13 22:13:39 +0530

    initial commit

--- js/07_Typeconversion.js
+++ js/07_Typeconversion.js
@@ -1,11 +1,11 @@
-const a="a";
-const b="4";
+const a='4';
+const b=4;
 
-console.log(Boolean(""));
-console.log(Boolean(a));
-// const c=Number(a);
-// const d=Number(b);
-// console.log(typeof a);
-// console.log(typeof b);
-// console.log(typeof c);
-// console.log(typeof d);
+// console.log(Boolean(""));
+// console.log(Boolean(b));
+const c=Number(a);
+const d=Number(b);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
