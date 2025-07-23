4be3e3d8be0fd565bc26956a4c9b391604043123
vaishnavi <vaishnavishruti121@gmail.com>
2025-07-23 22:20:55 +0530

    function that takes two numbers as input and print their sum

---
+++ practic.js/Day-10.js
@@ -0,0 +1,12 @@
+// Write a function that takes two numbers as input and prints their sum.
+
+
+function printsum(num1, num2){
+    let sum = num1 + num2
+    console.log(sum);
+}
+
+printsum(5, 7);
+
+
+
